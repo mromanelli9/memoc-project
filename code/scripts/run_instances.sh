d40ea322a595303cc5d2026f2286fe38f9108b71
Marco Romanelli <mromanelli9@gmail.com>
2017-05-30 17:34:00 +0200

    fix

--- code/scripts/run_instances.sh
+++ code/scripts/run_instances.sh
@@ -9,8 +9,11 @@
 
 INSTANCES_DIR="instances"
 MAIN=bin/main
-OUTFILE=target/results.txt
+OUTFILE_DIR="output"
+OUTFILE=$OUTFILE_DIR/results.txt
 
+mkdir "$OUTFILE_DIR"
+touch $OUTFILE
 for file in `ls $INSTANCES_DIR/*.tsp | sort -V`; do
 	echo "Running on $file.";
 	TIME=`/usr/bin/time -f "%U\t%S\t%x" $MAIN "$file" 2>&1`
